84675441aae81a7b642c977d0f2ba10666695487
kickinbahk <kickinbahk@gmail.com>
2016-02-05 18:28:46 -0800

    Add init in app.js, add script to start app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+var storage = require("node-persist");
+storage.initSync();
+
+// storage.setItemSync("name", "Josiah");
+
 console.log("Starting Password Manager");
+
+
+
+// var name = storage.getItemSync("name");
+// console.log(name);
