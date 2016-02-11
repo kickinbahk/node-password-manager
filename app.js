38d498199ca8106d3bf36139b5a810989abff2c3
kickinbahk <kickinbahk@gmail.com>
2016-02-10 19:25:33 -0800

    Take command line args for functionality

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 var storage = require("node-persist");
-
 storage.initSync();
 
+var argv = require("yargs")
+    .command("create", "Create a new account", function(yargs) {
+        yargs.options({
+            name: {
+                demand: true,
+                alias: 'n',
+                description: "Accout name (e.g: Twitter)",
+                type: "string"
+            },
+            username: {
+                demand: true,
+                alias: 'u',
+                description: "youremail@example.com",
+                type: "string"
+            },
+            password: {
+                demand: true,
+                alias: 'p',
+                description: "qwf123",
+                type: "string"
+            }
+        }).help("help");
+    })
+    .command("get", "Get an existing account", function(yargs){
+        yargs.options({
+            name: {
+                demand: true,
+                alias: 'n',
+                description: "Accout name (e.g: Twitter)",
+                type: "string"
+            }
+        }).help("help");
+    })
+    .help("help")
+    .argv;
+var command = argv._[0];
 // create
 //   --name
 //   -- username
@@ -36,6 +71,22 @@ function getAccount(accountName) {
     return foundUser;
 }
 
-//createAccount(sandy);
+if (command === "create") {
+    var createdAccount = createAccount({
+        name: argv.name,
+        username: argv.username,
+        password: argv.password        
+    });
+    console.log("Account Created!");
+    console.log(createdAccount);
+} else if (command === "get") {
+    var foundAccount = getAccount(argv.name);        
+    if (typeof foundAccount === 'undefined') {
+        console.log("Account Not Found");
+    } else {
+        console.log("Account Found!");
+        console.log(foundAccount);
+    }
+}
 
-getAccount("Facebook");
+
