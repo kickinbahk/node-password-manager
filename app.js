ccd976d8b65fd84a3ec8635f503df1b5354914a2
kickinbahk <kickinbahk@gmail.com>
2016-02-12 18:56:32 -0800

    add functions for new functionality

--- app.js
+++ app.js
@@ -2,54 +2,65 @@ var storage = require("node-persist");
 storage.initSync();
 
 var argv = require("yargs")
-    .command("create", "Create a new account", function(yargs) {
-        yargs.options({
-            name: {
-                demand: true,
-                alias: 'n',
-                description: "Accout name (e.g: Twitter)",
-                type: "string"
-            },
-            username: {
-                demand: true,
-                alias: 'u',
-                description: "youremail@example.com",
-                type: "string"
-            },
-            password: {
-                demand: true,
-                alias: 'p',
-                description: "qwf123",
-                type: "string"
-            }
-        }).help("help");
-    })
-    .command("get", "Get an existing account", function(yargs){
-        yargs.options({
-            name: {
-                demand: true,
-                alias: 'n',
-                description: "Accout name (e.g: Twitter)",
-                type: "string"
-            }
-        }).help("help");
-    })
-    .help("help")
-    .argv;
+        .command("create", "Create a new account", function(yargs) {
+            yargs.options({
+                name: {
+                    demand: true,
+                    alias: 'n',
+                    description: "Accout name (e.g: Twitter)",
+                    type: "string"
+                },
+                username: {
+                    demand: true,
+                    alias: 'u',
+                    description: "youremail@example.com",
+                    type: "string"
+                },
+                password: {
+                    demand: true,
+                    alias: 'p',
+                    description: "qwf123",
+                    type: "string"
+                },
+                masterPassword: {
+                    demand: true,
+                    alias: 'mp',
+                    description: "qwf123",
+                    type: "string"
+                }
+            }).help("help");
+        })
+        .command("get", "Get an existing account", function(yargs){
+            yargs.options({
+                name: {
+                    demand: true,
+                    alias: 'n',
+                    description: "Accout name (e.g: Twitter)",
+                    type: "string"
+                },
+                masterPassword: {
+                    demand: true,
+                    alias: 'mp',
+                    description: "qwf123",
+                    type: "string"
+                }
+            }).help("help");
+        })
+        .help("help")
+        .argv;
 var command = argv._[0];
-// create
-//   --name
-//   -- username
-//   password
 
-// get
-//   --name
+function getAccouts (masterPassword) {
 
-// account.name
-// account.username
-// account.password
 
-function createAccount(account) {
+}
+
+
+function saveAccounts (accounts, masterPassword) {
+
+}
+
+function createAccount(account, masterPassword) {
     var accounts = storage.getItemSync("accounts");
     if (typeof accounts === "undefined"){
         accounts = [];
@@ -59,7 +70,7 @@ function createAccount(account) {
     return account
 }
 
-function getAccount(accountName) {
+function getAccount(accountName, masterPassword) {
     var accounts = storage.getItemSync("accounts");
     var foundUser;
     accounts.forEach(function(account){        
@@ -75,12 +86,12 @@ if (command === "create") {
     var createdAccount = createAccount({
         name: argv.name,
         username: argv.username,
-        password: argv.password        
-    });
+        password: argv.password,       
+    }, argb.masterPassword);
     console.log("Account Created!");
     console.log(createdAccount);
 } else if (command === "get") {
-    var foundAccount = getAccount(argv.name);        
+    var foundAccount = getAccount(argv.name, argv.masterPassword);        
     if (typeof foundAccount === 'undefined') {
         console.log("Account Not Found");
     } else {
